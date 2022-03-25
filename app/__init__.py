0b8587578807df7ad32282a2c6eab24da6102a1d
Timoh97 <timohmugendi@gmail.com>
2022-03-25 14:57:13 +0300

    import config options in flask

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from config import config_options
+
+
+
+def create_app(config_name):
+   app = Flask(__name__)
+   
+   #create the app configurations
+   app.config.from_object(config_options[config_name])
+   config_options[config_name].init_app(app)
+   
+
+#import the blueprints
+from .main import main as main_blueprint
+from . auth import auth as auth_blueprint
+#register the blueprints
+app.register_blueprint(main_blueprint)
