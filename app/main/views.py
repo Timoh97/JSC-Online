a5f672a6db424c272bad785680a9c3cbe2150100
Timoh97 <timohmugendi@gmail.com>
2022-03-25 16:54:29 +0300

    add profile view function and profile.html

--- app/main/views.py
+++ app/main/views.py
@@ -7,3 +7,10 @@ def index():
 
   title = "Home page"
   return render_template('main/index.html',title=title)
+
+@main.route('/profile')
+def profile():
+  
+
+  title = "profile page"
+  return render_template('profile/profile.html',title=title)
