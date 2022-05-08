9bec36df160bc23bb60c59ca02aab6a0d4460277
Timoh97 <timohmugendi@gmail.com>
2022-05-09 02:06:24 +0300

    add application initialization configurations

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,16 @@ from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 bootstrap=Bootstrap()
 db=SQLAlchemy()
+# photos = UploadSet('photos',IMAGES)
 
+login_manager = LoginManager()
+ 
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -15,7 +21,11 @@ def create_app(config_name):
    app.config.from_object(config_options[config_name])
    config_options[config_name].init_app(app)
    
-
+   # Initializing flask extensions
+   db.init_app(app)
+   bootstrap.init_app(app)
+   login_manager.init_app(app)
+   
 #import the blueprints
    from .main import main as main_blueprint
    from . auth import auth as auth_blueprint
